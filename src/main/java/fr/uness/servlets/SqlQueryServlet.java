9884de2126fdf2c6d9ecd6d310c14604016af872
Adam <a.sanchez75@gmail.com>
2023-06-25 18:24:35 +0200

    generation columns automatically

--- src/main/java/fr/uness/servlets/SqlQueryServlet.java
+++ src/main/java/fr/uness/servlets/SqlQueryServlet.java
@@ -3,8 +3,11 @@ package fr.uness.servlets;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,16 +20,31 @@ public class SqlQueryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 			try {
+
 				String query = request.getParameter("sparql");
 				Connection conn = VirtuosoDatabase.getConnection();
 				Statement st = conn.createStatement();
 				ResultSet rs = st.executeQuery("sparql " + query); 	
-				while (rs.next()) {
-					String s =  rs.getString(1); 
-					String p =  rs.getString(2); 
-					String o =  rs.getString(3); 
-					System.out.println(s + "|" + p + "|" + o);
-				}
+				
+	            ResultSetMetaData m = rs.getMetaData();
+	            int ccount = m.getColumnCount();
+
+	            Map<String, String> map = new HashMap<String, String>();
+
+	            while (rs.next()) {
+	                for (int i = 1; i <= ccount; i++) {
+	                  map.put(m.getColumnName(i),rs.getString(i)); 
+	                }
+	            System.out.println(map.toString());
+	            }
+
+	            rs.close();
+
+	            st.close();
+
+	            conn.close();
+
+	            
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
